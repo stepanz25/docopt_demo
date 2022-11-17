81e1dd2426a5bb240733ae0fc7891f619132e125
Stepan Zaiatc <zaiatc.stepan@gmail.com>
2022-11-17 12:42:06 -0800

    added optional positional argument

--- demo.R
+++ demo.R
@@ -1,13 +1,14 @@
 # author: Tiffany Timbers
 # date: 2020-01-15
-# Comment
+#
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[<arg4>]          Takes any value (this is an optional positional argument)
 " -> doc
 
 library(docopt)
